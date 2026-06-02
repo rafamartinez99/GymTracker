380d5b5906cbf62bfc406b52ebf68b2b19d77a02
Rafael Martinez <rafamartineez99@gmail.com>
2026-06-02 12:13:29 +0200

    feat: agregar App.kt con HiltAndroidApp e inicialización Firebase

---
+++ app/src/main/java/es/rafamartinez/gymtracker/App.kt
@@ -0,0 +1,14 @@
+package es.rafamartinez.gymtracker
+
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.initialize(this)
+    }
+}
